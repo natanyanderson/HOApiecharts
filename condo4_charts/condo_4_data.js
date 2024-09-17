249a4a7712a5fd5608efa90db4e6d3f9e182d70a
Erik Anderson <eanderson20@bloomberg.net>
2024-09-17 14:07:00 -0400

    Taking a snapshot of a good working state.

---
+++ condo4_charts/condo_4_data.js
@@ -0,0 +1,473 @@
+define([], function() {
+    return {
+        2022: {
+            expenses: {
+                "Office expenses": 1738,
+                "HOA dues expense": 449232,
+                "HOA assessments": 58880,
+                "Professional fees": 3333,
+                "Utilities": 626,
+                "Insurance": 4159,
+                "Sprinklers": 9428,
+                "Powerwashing &\t\nDeck staining": 13579,
+                "Window Wells": 137210,
+                "Roof": 7143,
+                "Other repairs &\n\tmaintenance": 2242,
+                "Corporate Taxes": 650
+            },
+            income: {
+                "Revenue": null,
+                "Common Charges": 41240,
+                "Assessments": 41240,
+                "Assessments - Road Fund": 17640,
+                "Late Fees": 1700,
+                "Chargeback Income": 195,
+                "Interest Income": 3025,
+                "HOA dues income": 449232,
+                "Other revenue": 978,
+                "Total Revenue": 627000,
+                "Total Expenses": 688220,
+                "Net Profit (Loss)": -61220
+            },
+            assets: {
+                "Total Assets": 568989
+            }
+        },
+        2021: {
+            expenses: {
+                "Office expenses": 779,
+                "Professional Fees": 2900,
+                "Utilities": 695,
+                "Insurance": 4809,
+                "Sprinklers": 1328,
+                "Powerwashing & Deck staining": 40685,
+                "Window wells": 97234,
+                "Walkway": 11867,
+                "Roof": 7807,
+                "Other repairs and maintenance": 1907,
+                "Corporate Taxes": 647
+            },
+            income: {
+                "Revenue": null,
+                "Common Charges": 41239,
+                "Assessments": 431592,
+                "Assessments - Road Fund": 17640,
+                "Late Fees": 1600,
+                "Chargeback Income": 410,
+                "Interest Income": 1863,
+                "Total Revenue": 596353,
+                "Total Expenses": 661743,
+                "Net Profit (Loss)": -65390
+            },
+            assets: {
+                "Total Assets": 624272
+            }
+        },
+        2020: {
+            expenses: {
+                "Office expenses": 1283,
+                "HOA dues expense": 413952,
+                "HOA assessments": 58879,
+                "Professional fees": 2900,
+                "Utilities": 683,
+                "Insurance": 4869,
+                "Sprinklers": 3490,
+                "Powerwashing & Deck staining": 13161,
+                "Roof": 9212,
+                "Walkway": 11244,
+                "Other repairs and maintenance": 8226,
+                "Corporate Taxes": 856
+            },
+            income: {
+                "Revenue": null,
+                "Common Charges": 41239,
+                "Assessments": 413952,
+                "Assessments - Road Fund": 17640,
+                "Late Fees": 785,
+                "Chargeback Income": 3110,
+                "Interest Income": 6472,
+                "Total Revenue": 576004,
+                "Total Expenses": 531473,
+                "Net Profit": 44531
+            },
+            assets: {
+                "Total Assets": 699210
+            }
+        },
+        2019: {
+            expenses: {
+                "Office expenses": 1047,
+                "HOA dues expense": 390432,
+                "HOA assessments": 58879,
+                "Professional fees": 2900,
+                "Utilities": 143,
+                "Insurance": 3979,
+                "Sprinklers": 610,
+                "Powerwashing": 5310,
+                "Window wells": 13926,
+                "Roof": 28069,
+                "Other repairs and maintenance": 32675,
+                "Corporate Taxes": 1799
+            },
+            income: {
+                "Revenue": null,
+                "Common Charges": 41239,
+                "Assessments": 390432,
+                "Assessments - Road Fund": 17640,
+                "Late Fees": 1450,
+                "Chargeback Income": 2080,
+                "Interest Income": 5400,
+                "Total Revenue": 531243,
+                "Total Expenses": 551350,
+                "Net Profit (Loss)": -20107
+            },
+            assets: {
+                "Total Assets": 637048
+            }
+        },
+        2018: {
+            expenses: {
+                "Office expenses": 1095,
+                "HOA dues expense": 366912,
+                "HOA assessments": 58879,
+                "Professional fees": 2650,
+                "Utilities": 2746,
+                "Insurance": 6502,
+                "Powerwashing": 6361,
+                "Window wells": 50689,
+                "Roof": 592,
+                "Other repairs and maintenance": 5564,
+                "Corporate Taxes": 592
+            },
+            income: {
+                "Revenue": null,
+                "Common Charges": 41239,
+                "Assessments": 366912,
+                "Assessments - Road Fund": 17640,
+                "Late Fees": 1155,
+                "Chargeback Income": 2685,
+                "Interest Income": 3807,
+                "Total Revenue": 506222,
+                "Total Expenses": 454622,
+                "Net Profit": 51600
+            },
+            assets: {
+                "Total Assets": 648539
+            }
+        },
+        2017: {
+            expenses: {
+                "Office expenses": 1413,
+                "HOA dues expense": 366912,
+                "Professional fees": 3250,
+                "Utilities": 396,
+                "Insurance": 2988,
+                "Powerwashing": 7330,
+                "Roof": 6733,
+                "Other repairs and maintenance": 900,
+                "Corporate Taxes": 983
+            },
+            income: {
+                "Revenue": null,
+                "Common Charges": 44999,
+                "Assessments": 366912,
+                "Assessments - Road Fund": 1065,
+                "Late Fees": 1920,
+                "Chargeback Income": 2776,
+                "Interest Income": 2720,
+                "Total Revenue": 490539,
+                "Total Expenses": 436611,
+                "Income from operations": 53928,
+                "Net Profit": 13471
+            },
+            assets: {
+                "Total Assets": 586416
+            }
+        },
+        2016: {
+            expenses: {
+                "Office expenses": 1497,
+                "HOA dues expense": 366912,
+                "Professional fees": 3100,
+                "Utilities": 1295,
+                "Insurance": 885,
+                "Other repairs and maintenance": 2795,
+                "Corporate Taxes": 725
+            },
+            income: {
+                "Revenue": null,
+                "Common Charges": 41796,
+                "Assessments": 366912,
+                "Assessments - Road Fund": 3000,
+                "Late Fees": 1200,
+                "Chargeback Income": 3249,
+                "Interest Income": 3249,
+                "Total Revenue": 475000,
+                "Total Expenses": 398230,
+                "Income from operations": 50770,
+                "Net Profit": 50880
+            },
+            assets: {
+                "Total Assets": 569428
+            }
+        },
+        2015: {
+            expenses: {
+                "Office expenses": 1377,
+                "HOA dues expense": 415222,
+                "Professional fees": 2700,
+                "Utilities": 1675,
+                "Insurance": 2280,
+                "Powerwashing": 16386,
+                "Other repairs and maintenance": 26829,
+                "Corporate Taxes": 776
+            },
+            income: {
+                "Revenue": null,
+                "Common Charges": 41796,
+                "Assessments": 415222,
+                "Late Fees": 5475,
+                "Chargeback Income": 3717,
+                "Interest Income": 5483,
+                "Total Revenue": 466210,
+                "Total Expenses": 385188,
+                "Income from operations": 464604,
+                "Net Profit": 1606
+            },
+            assets: {
+                "Total Assets": 516722
+            }
+        },
+        2014: {
+            expenses: {
+                "Office expenses": 1632,
+                "HOA dues expense": 403412,
+                "Professional fees": 2400,
+                "Utilities": 2365,
+                "Insurance": 5610,
+                "Powerwashing": 6110,
+                "Other repairs and maintenance": 5810,
+                "Corporate Taxes": 661
+            },
+            income: {
+                "Revenue": null,
+                "Common Charges": 41303,
+                "Assessments": 403412,
+                "Late Fees": 2877,
+                "Chargeback Income": 3000,
+                "Interest Income": 2710,
+                "Total Revenue": 451169,
+                "Total Expenses": 372935,
+                "Income from operations": 61767,
+                "Net Profit": 61767
+            },
+            assets: {
+                "Total Assets": 512649
+            }
+        },
+        2013: {
+            expenses: {
+                "Office expenses": 1574,
+                "HOA dues expense": 403412,
+                "HOA assessments": 58879,
+                "Professional fees": 2400,
+                "Utilities": 2560,
+                "Insurance": 6375,
+                "Other repairs and maintenance": 14066,
+                "Corporate Taxes": 548
+            },
+            income: {
+                "Revenue": null,
+                "Common Charges": 403412,
+                "Assessments": 119400,
+                "Late Fees": 6212,
+                "Chargeback Income": 215,
+                "Interest Income": 529826,
+                "Total Revenue": 450982,
+                "Total Expenses": 469785,
+                "Income from operations": 60041,
+                "Net Profit": 60041
+            },
+            assets: {
+                "Total Assets": 450342
+            }
+        },
+        2012: {
+            expenses: {
+                "Office expenses": 1798,
+                "HOA dues expense": 403412,
+                "HOA assessments": 58879,
+                "Professional fees": 2821,
+                "Utilities": 1580,
+                "Insurance": 6232,
+                "Other repairs and maintenance": 8732,
+                "Corporate Taxes": 510
+            },
+            income: {
+                "Revenue": null,
+                "Common Charges": 403412,
+                "Late Fees": 5420,
+                "Interest Income": 2890,
+                "Total Revenue": 409634,
+                "Total Expenses": 450982,
+                "Income from operations": 60206,
+                "Net Profit": 60206
+            },
+            assets: {
+                "Total Assets": 376022
+            }
+        },
+        2011: {
+            expenses: {
+                "Office expenses": 1513,
+                "HOA dues expense": 390724,
+                "Professional fees": 2650,
+                "Utilities": 1320,
+                "Insurance": 6241,
+                "Other repairs and maintenance": 7298,
+                "Corporate Taxes": 483
+            },
+            income: {
+                "Revenue": null,
+                "Common Charges": 390724,
+                "Late Fees": 1285,
+                "Interest Income": 3200,
+                "Total Revenue": 395787,
+                "Total Expenses": 417782,
+                "Income from operations": -21995,
+                "Net Profit (Loss)": -21995
+            },
+            assets: {
+                "Total Assets": 316343
+            }
+        },
+        2010: {
+            expenses: {
+                "Office expenses": 1536,
+                "HOA dues expense": 368562,
+                "Professional fees": 2280,
+                "Utilities": 1425,
+                "Insurance": 6225,
+                "Other repairs and maintenance": 8952,
+                "Corporate Taxes": 399
+            },
+            income: {
+                "Revenue": null,
+                "Common Charges": 368562,
+                "Late Fees": 3280,
+                "Interest Income": 3295,
+                "Total Revenue": 375242,
+                "Total Expenses": 333009,
+                "Income from operations": 42233,
+                "Net Profit": 42233
+            },
+            assets: {
+                "Total Assets": 304752
+            }
+        },
+        2009: {
+            expenses: {
+                "Office expenses": 2350,
+                "HOA dues expense": 356412,
+                "Professional fees": 2450,
+                "Utilities": 1865,
+                "Insurance": 6245,
+                "Other repairs and maintenance": 8932,
+                "Corporate Taxes": 327
+            },
+            income: {
+                "Revenue": null,
+                "Common Charges": 356412,
+                "Late Fees": 2708,
+                "Interest Income": 2855,
+                "Total Revenue": 367723,
+                "Total Expenses": 326581,
+                "Income from operations": 41142,
+                "Net Profit": 41142
+            },
+            assets: {
+                "Total Assets": 256856
+            }
+        },
+        2008: {
+            expenses: {
+                "Office expenses": 3140,
+                "HOA dues expense": 327434,
+                "Professional fees": 2600,
+                "Utilities": 2650,
+                "Insurance": 5730,
+                "Other repairs and maintenance": 8435,
+                "Corporate Taxes": 391
+            },
+            income: {
+                "Revenue": null,
+                "Common Charges": 327434,
+                "Late Fees": 2999,
+                "Interest Income": 3215,
+                "Total Revenue": 333303,
+                "Total Expenses": 300898,
+                "Income from operations": 32405,
+                "Net Profit": 32405
+            },
+            assets: {
+                "Total Assets": 210539
+            }
+        },
+        2007: {
+            expenses: {
+                "Office expenses": 6686,
+                "HOA dues expense": 327434,
+                "Professional fees": 6734,
+                "Utilities": 6320,
+                "Insurance": 7284,
+                "Other repairs and maintenance": 8500,
+                "Corporate Taxes": 397
+            },
+            income: {
+                "Revenue": null,
+                "\tCommon Charges and HOA dues": 327434,
+                "\tLate Fees and sundry": 5937,
+                "\tInterest Income": 3385,
+                "Total Revenue": 426498,
+                "Total Expenses": 478336,
+                "Income from operations": -51838,
+                "Net Profit (Loss)": -51838
+            },
+            assets: {
+                "Total Assets": 173618
+            }
+        },
+        2006: {
+            expenses: {
+                "Office expenses": 4052,
+                "HOA dues expense": 327434,
+                "Professional fees": 6722,
+                "Utilities": 6125,
+                "Insurance": 7685,
+                "Other repairs and maintenance": 8720,
+                "Corporate Taxes": 400
+            },
+            income: {
+                "Revenue": null,
+                "\tCommon Charges and HOA dues": 327434,
+                "\tLate Fees": 3872,
+                "\tInterest Income": 3645,
+                "Total Revenue": 344015,
+                "Total Expenses": 302543,
+                "Income from operations": 41472,
+                "Net Profit (Loss)": 41472
+            },
+            assets: {
+                "Total Assets": 221000
+            }
+        }
+    };
+});
+
+
+
+
+
+
+
+
