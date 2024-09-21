cdb5486b7dad99f38c8ad7f310bdb6aacd20cd71
Erik Anderson <eanderson20@bloomberg.net>
2024-09-21 19:48:36 -0400

    Adding Condo 4 analytics.

---
+++ condo4_charts/condo_4_schedule.js
@@ -0,0 +1,242 @@
+define([], function() {
+    return {
+        colors: [
+            '#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe',
+            '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080',
+            '#ffffff', '#000000', '#19a9e6', '#f4e619', '#d86343', '#b491e6', '#f0a146', '#31f0f0', '#e632f0', '#6cf60c',
+            '#bebebe', '#800860', '#e6ffbe', '#63429a', '#c8facc', '#000800', '#aac3ff', '#8000ff', '#b1ffd8', '#750000',
+            '#606080', '#cfcfcf', '#a919e6', '#194ee6', '#31f46f', '#e619c3', '#46f0b4', '#f0329c', '#bcf06c', '#fabcbe'
+        ],
+        yearly: {
+            2022: {
+                maintenance: {
+                    "Driveway-asphalt reconstruction": 170505
+                },
+                expenses: 170505,
+                full_contribution: 343139
+            },
+            2023: {
+                maintenance: {
+                },
+                full_contribution: 343139
+            },
+            2024: {
+                maintenance: {
+                    "Walkway-entry stoop, end unit porch": 10609,
+                    "Walkway-entry walk/stair/landing, concrete": 3533
+                },
+                expenses: 14142,
+                full_contribution: 343139
+            },
+            2025: {
+                maintenance: {
+                },
+                expenses: 0,
+                full_contribution: 343139
+            },
+            2026: {
+                maintenance: {
+                    "BUILDING-Deck-wood reconstruction": 98324,
+                    "BUILDING-Retaining Wall-wood tie wells": 16883
+                },
+                expenses: 115207,
+                full_contribution: 343139
+            },
+            2027: {
+                maintenance: {
+                    "BUILDING-Roof-shingles, asphalt incl gutters/leaders-": 1145131,
+                    "BUILDING-Deck-wood reconstruction": 104312,
+                    "SITE WORK-Walkway-entry stoop, end unit porch": 12299,
+                    "BUILDING-Retaining Wall-wood tie wells": 16883,
+                    "BUILDING-Deck-wood reconstruction": 101274,
+                    "BUILDING-Roof-chimney caps, large": 45444,
+                    "BUILDING-Roof-chimney caps, small": 15824,
+                    "SITE WORK-Driveway-asphalt seal coat": 10981,
+                    "SITE WORK-Walkway-entry<br>walk/stair/landing, concrete": 3860
+                },
+                expenses: 1322514,
+                full_contribution: 343139
+            },
+            2028: {
+                maintenance: {
+                    "BUILDING-Deck-wood reconstruction": 104312
+                },
+                expenses: 104312,
+                full_contribution: 262965
+            },
+            2029: {
+                maintenance: {
+                    "SITE WORK-Walkway-entry stoop, end unit porch": 12299
+                },
+                expenses: 12299,
+                full_contribution: 262965
+            },
+            2030: {
+                maintenance: {
+                    "SITE WORK-Walkway-entry walk/stair/landing, concrete": 4218
+                },
+                expenses: 4218,
+                full_contribution: 262965
+            },
+            2031: {
+                maintenance: {
+                },
+                full_contribution: 262965
+            },
+            2032: {
+                maintenance: {
+                    "SITE WORK-Driveway-asphalt seal coat": 12730
+                },
+                expenses: 12730,
+                full_contribution: 262965
+            },
+            2033: {
+                maintenance: {
+                    "SITE WORK-Walkway-entry walk/stair/landing, concrete": 4609
+                },
+                expenses: 4609,
+                full_contribution: 263962
+            },
+            2034: {
+                maintenance: {
+                    "SITE WORK-Walkway-entry stoop, end unit porch": 14258
+                },
+                expenses: 14258,
+                full_contribution: 263962
+            },
+            2035: {
+                maintenance: {
+                    "BUILDING-Fence-privacy, vinyl": 11359
+                },
+                expenses: 11359,
+                full_contribution: 263962
+            },
+            2036: {
+                maintenance: {
+                    "BUILDING-Retaining Wall-wood tie wells": 22689,
+                    "SITE WORK-Walkway-entry walk/stair/landing, concrete": 5037
+                },
+                expenses: 27726,
+                full_contribution: 263962
+            },
+            2037: {
+                maintenance: {
+                    "SITE WORK-Driveway-asphalt reconstruction": 265641
+                },
+                expenses: 265641,
+                full_contribution: 263962
+            },
+            2038: {
+                maintenance: {
+                },
+                full_contribution: 275331
+            },
+            2039: {
+                maintenance: {
+                    "SITE WORK-Walkway-entry stoop, end unit porch": 16528,
+                    "SITE WORK-Walkway-entry walk/stair/landing, concrete": 5504
+                },
+                expenses: 22032,
+                full_contribution: 275331
+            },
+            2040: {
+                maintenance: {
+                },
+                full_contribution: 275331
+            },
+            2041: {
+                maintenance: {
+                },
+                full_contribution: 275331
+            },
+            2042: {
+                maintenance: {
+                    "SITE WORK-Driveway-asphalt seal coat": 17108,
+                    "SITE WORK-Walkway-entry walk/stair/landing, concrete": 6014
+                },
+                expenses: 23123,
+                full_contribution: 275331
+            },
+            2043: {
+                maintenance: {
+                },
+                full_contribution: 275331
+            },
+            2044: {
+                maintenance: {
+                    "SITE WORK-Walkway-entry stoop, end unit porch": 19161
+                },
+                expenses: 19161,
+                full_contribution: 275331
+            },
+            2045: {
+                maintenance: {
+                    "SITE WORK-Walkway-entry walk/stair/landing, concrete": 6572
+                },
+                expenses: 6572,
+                full_contribution: 275331
+            },
+            2046: {
+                maintenance: {
+                    "BUILDING-Retaining Wall-wood tie wells": 30492
+                },
+                expenses: 30492,
+                full_contribution: 275331
+            },
+            2047: {
+                maintenance: {
+                    "SITE WORK-Driveway-asphalt seal coat": 19833
+                },
+                expenses: 19833,
+                full_contribution: 275331
+            },
+            2048: {
+                maintenance: {
+                    "SITE WORK-Walkway-entry walk/stair/landing, concrete": 7181
+                },
+                expenses: 7181,
+                full_contribution: 282019
+            },
+            2049: {
+                maintenance: {
+                    "SITE WORK-Walkway-entry stoop, end unit porch": 22213
+                },
+                expenses: 22213,
+                full_contribution: 282019
+            },
+            2050: {
+                maintenance: {
+                },
+                full_contribution: 282019
+            },
+            2051: {
+                maintenance: {
+                    "BUILDING-Deck-wood reconstruction": 205870,
+                    "SITE WORK-Walkway-entry walk/stair/landing, concrete": 7847
+                },
+                expenses: 213717,
+                full_contribution: 282019
+            },
+            2052: {
+                maintenance: {
+                    "BUILDING-Siding-vinyl": 3598659,
+                    "SITE WORK-Driveway-asphalt reconstruction": 413860,
+                    "BUILDING-Deck-wood reconstruction": 212046 
+                },
+                expenses: 4224565,
+                full_contribution: 282019
+            }
+        },
+        five_threshold_contribution: 209125,
+        ten_threahold_contribution: 236504,
+        starting_balance_2022: 582976
+    };
+});
+
+
+
+
+
+
+
+
