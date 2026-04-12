4f612ee4fb59161ed539d4fde1a4365374b42740
Natanya Anderson <natanyaanders@gmail.com>
2026-04-12 11:28:30 -0400

    Removed password so I can add to HOA website.

---
+++ HOA_pie_charts/hoa_schedule.js
@@ -0,0 +1,294 @@
+define([], function() {
+    return {
+        colors: [
+            '#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe',
+            '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080',
+            '#ffffff', '#000000', '#19a9e6', '#f4e619', '#d86343', '#b491e6', '#f0a146', '#31f0f0', '#e632f0', '#6cf60c',
+            '#bebebe', '#800860', '#e6ffbe', '#63429a', '#c8facc', '#000800', '#aac3ff', '#8000ff', '#b1ffd8', '#750000',
+            '#606080', '#cfcfcf', '#a919e6', '#194ee6', '#31f46f', '#e619c3', '#46f0b4', '#f0329c', '#bcf06c', '#fabcbe'
+        ],
+        yearly: {
+            2022: {
+                maintenance: {
+                    "Clubhouse-hot water heater": 3000,
+                    "Driveway-asphalt reconstruct, emergency access": 7200,
+                    "Driveway-asphalt reconstruction": 429856,
+                    "Roadway-granite block entry": 37875,
+                    "Walkway-concrete": 6314
+                },
+                expenses: 484245
+            },
+            2023: {
+                maintenance: {
+                    "Fence-split rail, replacement fund": 3863
+                },
+                expenses: 3863
+            },
+            2024: {
+                maintenance: {
+                    "Clubhouse-fitness equipment": 5305,
+                    "Swimming Pool-concrete apron/deck": 42894,
+                    "Swimming Pool-coping": 11882,
+                    "Swimming Pool-cover": 4244,
+                    "Swimming Pool-pump": 2652,
+                    "Irrigation-repair fund": 5305
+                },
+                expenses: 72281
+            },
+            2025: {
+                maintenance: {
+                    "Swimming Pool-pool shell resurface incl tile": 24717,
+                    "Walkway-concrete": 6899
+                },
+                expenses: 31617
+            },
+            2026: {
+                maintenance: {
+                    "Fence-split rail, replacement fund": 4221,
+                    "": 0
+                },
+                expenses: 4221
+            },
+            2027: {
+                maintenance: {
+                    "Driveway-asphalt seal coat, emergency access": 464,
+                    "Refuse-concrete slab": 2782,
+                    "Roadway-asphalt seal coat": 38931
+                },
+                expenses: 42177
+            },
+            2028: {
+                maintenance: {
+                    "Clubhouse-furniture fund": 8955,
+                    "Swimming Pool-chlorination equipment": 1791,
+                    "Mailbox-double, pedestal and box": 18985,
+                    "Mailbox-single, pedestal and box": 2866,
+                    "Walkway-concrete": 7539
+                },
+                expenses: 40137
+            },
+            2029: {
+                maintenance: {
+                    "Clubhouse-fitness equipment": 6149,
+                    "Clubhouse-security, cctv": 2460,
+                    "Clubhouse-security, key fob entry system": 1845,
+                    "Swimming Pool-furniture fund": 14758,
+                    "Electrical-light pole/fixture": 112164,
+                    "Fence-split rail, replacement fund": 4612
+                },
+                expenses: 141989
+            },
+            2030: {
+                maintenance: {
+                    "Clubhouse-fire suppression system valve/pipe fund": 9501,
+                    "Clubhouse-flooring, carpet": 6613,
+                    "Clubhouse-hvac, humidifier": 3167,
+                    "Clubhouse-hvac, split system ac, 3.5 ton": 11401,
+                    "Fence-vinyl on masonry wall, 2ft": 17671,
+                    "Retaining Wall-masonry, along entry": 6334,
+                    "Signage-stop signs and misc informational signs": 11148,
+                    "Signage-street signs, wood": 3294
+                },
+                expenses: 69128
+            },
+            2031: {
+                maintenance: {
+                    "Clubhouse-roof shingles, asphalt incl gutters/leaders": 16440,
+                    "Clubhouse-roof, chimney cap": 1044,
+                    "Clubhouse-skylights": 5480,
+                    "Signage-flagpole": 3914,
+                    "Walkway-concrete": 8238
+                },
+                expenses: 35117
+            },
+            2032: {
+                maintenance: {
+                    "Driveway-asphalt seal coat, emergency access": 538,
+                    "Fence-split rail, replacement fund": 5040,
+                    "Roadway-asphalt seal coat": 45132
+                },
+                expenses: 50709
+            },
+            2033: {
+                maintenance: {
+                },
+                expenses: 0
+            },
+            2034: {
+                maintenance: {
+                    "Clubhouse-fitness equipment": 7129,
+                    "Swimming Pool-cover": 5703,
+                    "Swimming Pool-fence, aluminum": 26270,
+                    "Swimming Pool-pump": 3564,
+                    "Fence-board on board, wood at property line": 53252,
+                    "Irrigation-repair fund": 7129,
+                    "Walkway-concrete": 9002
+                },
+                expenses: 112049
+            },
+            2035: {
+                maintenance: {
+                    "Swimming Pool-pool shell resurface incl tile": 33218,
+                    "Fence-split rail, replacement fund": 5507
+                },
+                expenses: 38725
+            },
+            2036: {
+                maintenance: {
+                },
+                expenses: 0
+            },
+            2037: {
+                maintenance: {
+                    "Clubhouse-hot water heater, 75 gallon": 4674,
+                    "Driveway-asphalt reconstruct, emergency access": 11217,
+                    "Electrical-bollard lights, clubhouse walk": 7478,
+                    "Refuse-concrete slab": 3590,
+                    "Refuse-dumpsters": 3739,
+                    "Refuse-enclosure, wood gate/fence": 2804,
+                    "Roadway-asphalt resurface": 669702,
+                    "Walkway-concrete": 9837,
+                    "Walkway-sidewalk, flagstone at clubhouse": 8725
+                },
+                expenses: 721766
+            },
+            2038: {
+                maintenance: {
+                    "Clubhouse-door, main entry": 5616,
+                    "Clubhouse-flooring, ceramic tile": 4910,
+                    "Clubhouse-kitchen appliances": 3209,
+                    "Clubhouse-kitchenette refurbish": 9628,
+                    "Clubhouse-mirrored panels, fitness room": 3370,
+                    "Clubhouse-restroom/lockers": 48141,
+                    "Clubhouse-wallpaper": 5854,
+                    "Clubhouse-window treatments": 3209,
+                    "Clubhouse-windows": 12035,
+                    "Playground-equipment": 43327,
+                    "Playground-picnic table": 2086,
+                    "Playground-trash receptacle": 1284,
+                    "Swimming Pool-chlorination equipment": 2407,
+                    "Fence-guard rail": 17459,
+                    "Fence-split rail, replacement fund": 6018
+                },
+                expenses: 168555
+            },
+            2039: {
+                maintenance: {
+                    "Clubhouse-fitness equipment": 8264,
+                    "Swimming Pool-furniture fund": 19834
+                },
+                expenses: 28098
+            },
+            2040: {
+                maintenance: {
+                    "Signage-entry sign, large": 12768,
+                    "Signage-entry sign, small": 6810,
+                    "Walkway-concrete": 10749
+                },
+                expenses: 30327
+            },
+            2041: {
+                maintenance: {
+                    "Fence-split rail, replacement fund": 6576
+                },
+                expenses: 6576
+            },
+            2042: {
+                maintenance: {
+                    "Clubhouse-flooring, carpet": 9428,
+                    "Driveway-asphalt seal coat, emergency access": 722,
+                    "Roadway-asphalt seal coat": 60654
+                },
+                expenses: 70804
+            },
+            2043: {
+                maintenance: {
+                    "Swimming Pool-filters": 9301,
+                    "Walkway-concrete": 11746
+                },
+                expenses: 21047
+            },
+            2044: {
+                maintenance: {
+                    "Clubhouse-fitness equipment": 9581,
+                    "Clubhouse-security, cctv": 3832,
+                    "Clubhouse-security, key fob entry system": 2874,
+                    "Swimming Pool-coping": 21460,
+                    "Swimming Pool-cover": 7664,
+                    "Swimming Pool-pump": 4790,
+                    "Fence-split rail, replacement fund": 7185,
+                    "Irrigation-repair fund": 9581
+                },
+                expenses: 66968
+            },
+            2045: {
+                maintenance: {
+                    "Swimming Pool-pool shell resurface incl tile": 44643
+                },
+                expenses: 44643
+            },
+            2046: {
+                maintenance: {
+                    "Clubhouse-siding, vinyl": 32728,
+                    "Walkway-concrete": 12835
+                },
+                expenses: 45563
+            },
+            2047: {
+                maintenance: {
+                    "Driveway-asphalt seal coat, emergency access": 838,
+                    "Fence-split rail, replacement fund": 7852,
+                    "Refuse-dumpsters": 5025,
+                    "Roadway-asphalt seal coat": 70314
+                },
+                expenses: 84029
+            },
+            2048: {
+                maintenance: {
+                    "Clubhouse-furniture fund": 16174,
+                    "Swimming Pool-chlorination equipment": 3235,
+                    "Mailbox-double, pedestal and box": 34290,
+                    "Mailbox-single, pedestal and box": 5176
+                },
+                expenses: 58875
+            },
+            2049: {
+                maintenance: {
+                    "Clubhouse-fitness equipment": 11106,
+                    "Swimming Pool-furniture fund": 26655,
+                    "Walkway-concrete": 14025
+                },
+                expenses: 51787
+            },
+            2050: {
+                maintenance: {
+                    "Fence-split rail, replacement fund": 8580
+                },
+                expenses: 8580
+            },
+            2051: {
+                maintenance: {
+                },
+                expenses: 0
+            },
+            2052: {
+                maintenance: {
+                    "Clubhouse-hot water heater, 75 gallon": 7282,
+                    "Driveway-asphalt reconstruct, emergency access": 17476,
+                    "Roadway-asphalt resurface": 1043373,
+                    "Roadway-granite block entry": 91933,
+                    "Walkway-concrete": 15326
+                },
+                expenses: 1175390
+            }
+        }
+    };
+});
+
+
+
+
+
+
+
+
